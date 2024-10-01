1f5f07c5ff40e373445eae756d8b17127826ecf5
ducteo2003 <103983920+ducteo2003@users.noreply.github.com>
2024-10-01 11:55:25 +0700

    interface/DetailsProfileCustomer

---
+++ UngDungDatDichVuDonDep/app/src/main/java/com/example/happyhomes/Customer/ProfileDetailCustomer.java
@@ -0,0 +1,26 @@
+package com.example.happyhomes.Customer;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.happyhomes.R;
+
+public class ProfileDetailCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile_detail_customer);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
