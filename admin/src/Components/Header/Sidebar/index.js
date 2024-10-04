16988fbf38dcd5797f56f663890bedda0ebaaac8
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-10-04 13:59:54 +0700

    Done design home admin

--- admin/src/Components/Header/Sidebar/index.js
+++ admin/src/Components/Header/Sidebar/index.js
@@ -1,45 +1,82 @@
-import { Link } from "react-router-dom";
+import { AiOutlineAppstoreAdd } from "react-icons/ai";
+import { FaUserFriends, FaUserTie } from "react-icons/fa";
+import { FaHandsHoldingChild } from "react-icons/fa6";
+import { IoMdHome } from "react-icons/io";
+import { PiChatsFill } from "react-icons/pi";
+import "../../../App.css";
 import Logo from "../../../assets/images/logo-rebg.png";
+import { CiSearch } from "react-icons/ci";
+
 const Sidebar = () => {
   return (
-    <div className="bg-dark text-white p-3 vh-100">
-      <div className="text-center mb-4">
-        <img src={Logo} alt="Logo" style={{ width: "50px", height: "50px" }} />
-      </div>
-      <h4 className="text-center mb-4">NAME FULLNAME</h4>
-      <ul className="list-unstyled">
-        <li className="mb-3">
-          <Link to="/" className="text-white">
-            Trang chủ
-          </Link>
-        </li>
-        <li className="mb-3">
-          <Link to="/nhan-vien" className="text-white">
-            Nhân viên
-          </Link>
-        </li>
-        <li className="mb-3">
-          <Link to="/khach-hang" className="text-white">
-            Khách hàng
-          </Link>
-        </li>
-        <li className="mb-3">
-          <a href="#" className="text-white">
-            Dịch vụ
-          </a>
-        </li>
-        <li className="mb-3">
-          <a href="#" className="text-white">
-            Chat
-          </a>
-        </li>
-        <li className="mb-3">
-          <a href="#" className="text-white">
-            Khác
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <aside className="bg-indigo-900 text-white w-1/5 p-4 min-w-[200px] left-0">
+        <div className="flex items-center justify-center mb-8">
+          <div className="w-40 h-40 bg-white rounded-full flex items-center justify-center">
+            <img
+              className="logo w-32 h-auto text-indigo-900"
+              src={Logo}
+              alt="Trang quản lý HappyHome"
+            />
+          </div>
+        </div>
+        <div className="justify-center flex items-center ">
+          <span className="fullname text-center">Siêu Nguyễn Thanh</span>
+        </div>
+        <nav className="sidebar">
+          <ul>
+            <li className="mb-3 jus">
+              <a href="/home" className="flex items-center ml-4 p-2">
+                <i className="fas fa-home mr-2">
+                  <IoMdHome />
+                </i>
+                <p>Trang chủ</p>
+              </a>
+            </li>
+            <li className="mb-3 jus">
+              <a href="/home" className="flex items-center ml-4 p-2">
+                <i className="fas fa-home mr-2">
+                  <FaUserFriends />
+                </i>
+                <p>Nhân viên</p>
+              </a>
+            </li>
+            <li className="mb-3 jus">
+              <a href="/home" className="flex items-center ml-4 p-2">
+                <i className="fas fa-home mr-2">
+                  <FaUserTie />
+                </i>
+                <p>Khách hàng</p>
+              </a>
+            </li>
+            <li className="mb-3 jus">
+              <a href="/home" className="flex items-center ml-4 p-2">
+                <i className="fas fa-home mr-2">
+                  <FaHandsHoldingChild />
+                </i>
+                <p>Dịch vụ</p>
+              </a>
+            </li>
+            <li className="mb-3 jus">
+              <a href="/home" className="flex items-center ml-4 p-2">
+                <i className="fas fa-home mr-2">
+                  <PiChatsFill />
+                </i>
+                <p>Chat</p>
+              </a>
+            </li>
+            <li className="mb-3 jus">
+              <a href="/home" className="flex items-center ml-4 p-2">
+                <i className="fas fa-home mr-2">
+                  <AiOutlineAppstoreAdd />
+                </i>
+                <p>Khác</p>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
   );
 };
 
