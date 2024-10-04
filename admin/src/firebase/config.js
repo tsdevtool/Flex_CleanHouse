3d4bc1f0b04a064168110b64a00b4cd2e08efb72
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-10-04 11:44:56 +0700

    Done login feature and login design

--- admin/src/firebase/config.js
+++ admin/src/firebase/config.js
@@ -2,12 +2,6 @@
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import getDatabase from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCspLsug5m3JKwarri0Jis4mrLRSwIWnHs",
   authDomain: "flexcleanhouse.firebaseapp.com",
@@ -18,12 +12,8 @@ const firebaseConfig = {
   appId: "1:955476236687:web:4f9099599496579e2313fa",
   measurementId: "G-LCNW8T825E",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 const auth = getAuth(app);
-const database = getDatabase(app);
-
-export { analytics, app, auth, database };
+export { analytics, auth };
